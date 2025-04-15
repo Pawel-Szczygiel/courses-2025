e99cd1e244eeb71920a9db42b828b02ca8b8be1c
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2025-04-15 21:52:40 +0200

    read more , read less

--- src/Course.js
+++ src/Course.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Course = ({ name, info, image, price }) => {
-  
+    const [readMore, setReadMore] = useState(false);
+
+
     return (
     <article className='single-course'>
         <img src={image} alt={name}/>
@@ -10,6 +12,17 @@ const Course = ({ name, info, image, price }) => {
                 <h4>{name}</h4>
                 <h4 className='course-price'>${price}</h4>
             </div>
+            <p>
+                {
+                    readMore ? info : `${info.slice(0,100)}...`
+                }
+                <button onClick={() => setReadMore(!readMore)}>
+                {
+                    readMore ? 'read less' : 'read more'
+                }
+                </button>
+            </p>
+            <button className='delete-btn'>not interested</button>
         </footer>
     </article>
   )
